15412a7d3d8e3297c0cab06a88edc83250337cfe
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-19 17:44:11 +0100

    :sparkles: (module) implement module

--- src/module.ts
+++ src/module.ts
@@ -1,27 +1,48 @@
-import { resolve } from 'path'
-import defu from 'defu'
 import { Module } from '@nuxt/types'
+import { MotionVariants } from '@vueuse/motion'
+import defu from 'defu'
+import { resolve } from 'path'
 
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  directives?: {
+    [key: string]: MotionVariants
+  }
+  excludePresets?: boolean
+}
+
 const DEFAULTS: ModuleOptions = {}
-const CONFIG_KEY = 'myModule'
 
-const nuxtModule: Module<ModuleOptions> = /* async */ function (moduleOptions) {
-  const options = defu<ModuleOptions>(this.options[CONFIG_KEY], moduleOptions, DEFAULTS)
+const CONFIG_KEY = 'motions'
+
+const nuxtModule: Module<ModuleOptions> = function (moduleOptions) {
+  const options = defu<ModuleOptions>(
+    this.options[CONFIG_KEY],
+    moduleOptions,
+    DEFAULTS
+  )
   // const { nuxt } = this
 
-  this.addPlugin({
-    src: resolve(__dirname, '../templates/plugin.js'),
-    fileName: 'myPlugin.js',
+  this.addTemplate({
+    fileName: 'motions.js',
+    src: resolve(__dirname, '../templates', 'options.js'),
     options
+  })
+
+  this.addPlugin({
+    src: resolve(__dirname, '../templates', 'plugin.js'),
+    fileName: 'nuxt-use-motion.js'
   })
 }
 
 ;(nuxtModule as any).meta = require('../package.json')
 
 declare module '@nuxt/types' {
-  interface NuxtConfig { [CONFIG_KEY]?: ModuleOptions } // Nuxt 2.14+
-  interface Configuration { [CONFIG_KEY]?: ModuleOptions } // Nuxt 2.9 - 2.13
+  interface NuxtConfig {
+    [CONFIG_KEY]?: ModuleOptions;
+  } // Nuxt 2.14+
+  interface Configuration {
+    [CONFIG_KEY]?: ModuleOptions;
+  } // Nuxt 2.9 - 2.13
 }
 
 export default nuxtModule
