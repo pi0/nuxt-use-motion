4cd8bec545760da3875c2f498e0c143fcf8c790d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-19 23:12:03 +0100

    :sparkles: (example) implement basic example

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    Works!
+  <div class="container">
+    <div class="block" v-motion-slide-bottom />
   </div>
 </template>
 
-<script>
-export default {
-
+<style>
+.container {
+  width: 400px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid blue;
 }
-</script>
+
+.block {
+  width: 50px;
+  height: 50px;
+  background-color: red;
+}
+</style>
