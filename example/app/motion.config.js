3397bd11e3811082f08ce0703121bbe57fb93808
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-23 23:30:57 +0100

    :sparkles: (demo) small example for motion.config.js

--- example/app/motion.config.js
+++ example/app/motion.config.js
@@ -1,3 +1,14 @@
 export default {
-  test: 123
+  directives: {
+    'slide-rotate-bottom': {
+      initial: {
+        y: 200,
+        opacity: 0
+      },
+      enter: {
+        y: 0,
+        opacity: 1
+      }
+    }
+  }
 }
